effb9dd097c5de52ba97b53c553d10b1500d4121
inne <mrbelongtim@gmail.com>
2024-01-28 21:52:49 +0100

    Webpack configuration has been created

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Вернули CleanWebpackPlugin
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CleanWebpackPlugin(), // Вернули CleanWebpackPlugin
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerWebpackPlugin(),
+    ],
+  },
+  watch: true,
+};
